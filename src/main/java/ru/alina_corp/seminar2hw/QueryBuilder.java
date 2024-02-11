951f88611143463edded88dd01c104df5c8dc4a7
Alina N <my_work_space@bk.ru>
2024-02-11 09:58:04 +0300

    add hw3 theme anamal

--- src/main/java/ru/alina_corp/seminar2hw/QueryBuilder.java
+++
@@ -1,7 +0,0 @@
-package ru.alina_corp.seminar2hw;
-
-public class QueryBuilder {
-    public String buildDeleteQuery(String table, int id) {
-        return "DELETE FROM " + table + " WHERE ID = '" + id + "'";
-    }
-}
